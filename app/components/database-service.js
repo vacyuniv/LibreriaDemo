752a9d0961b2d6fc4a6c684ca1d1ce2d8340a6fe
matteo <matteo@matteoDebian>
2017-07-15 11:59:36 +0200

    Populated DB with a first set of mocked content

--- app/components/database-service.js
+++ app/components/database-service.js
@@ -9,31 +9,30 @@ function DbManager($log, $filter){
   dbReferences = {}
   dbReferences.db = undefined;
   // Table references
-  dbReferences.userTable_   = undefined;
-  dbReferences.authorTable_ = undefined;
-  dbReferences.bookTable_   = undefined;
+  dbReferences.userTable   = undefined;
+  dbReferences.authorTable = undefined;
+  dbReferences.bookTable   = undefined;
 
 
   // DB Creation
   this.initDb = function(){
     var schemaBuilder = lf.schema.create('libreriaDemo', 1);
 
-    // Schema declaration of the 3 tables
+    // Schema declaration of the 3 tables:
+    // --- User Table ----
     schemaBuilder.createTable('User')
       .addColumn('id', lf.Type.INTEGER)
       .addColumn('username', lf.Type.STRING)
       .addColumn('password', lf.Type.STRING)
       .addPrimaryKey(['id'], true) // true means its autoincremental
       .addIndex('idxUsername', ['username'], false, lf.Order.DESC);
-
-    // Schema declaration of the 3 tables
+    // --- Author Table ----
     schemaBuilder.createTable('Author')
       .addColumn('id', lf.Type.INTEGER)
       .addColumn('name', lf.Type.STRING)
       .addColumn('pseudonym', lf.Type.STRING)
       .addPrimaryKey(['id'], true); // true means its autoincremental
-
-    // Schema declaration of the 3 tables
+    // --- Book Table ----
     schemaBuilder.createTable('Book')
       .addColumn('isbn', lf.Type.STRING)
       .addColumn('title', lf.Type.STRING)
@@ -47,29 +46,82 @@ function DbManager($log, $filter){
       })
       .addPrimaryKey(['isbn']);
 
+    // ---- Connection to DB and populate the tables -------
     schemaBuilder.connect().then(function(db){
 
-      // Add references
+      // Add references for queries
       dbReferences.db           = db;
-      dbReferences.userTable_   = db.getSchema().table('User');
-      dbReferences.authorTable_ = db.getSchema().table('Author');
-      dbReferences.bookTable_   = db.getSchema().table('Book');
+      dbReferences.userTable   = db.getSchema().table('User');
+      dbReferences.authorTable = db.getSchema().table('Author');
+      dbReferences.bookTable   = db.getSchema().table('Book');
 
-      var row1 = dbReferences.userTable_.createRow({
-        'id': 1,
-        'username': 'try',
-        'password':'me'
-      });
-      var row2 = dbReferences.userTable_.createRow({
-        'username': 'try2',
-        'password':'me2'
-      });
-      var userRows = [row1, row2];
+      // --- Populate User table ----------------------------
+      var populateUsers = function pupulateUsers(){
+        var row1 = dbReferences.userTable.createRow({
+          'id': 1,
+          'username': 'try',
+          'password':'me'
+        });
+        var row2 = dbReferences.userTable.createRow({
+          'id': 2,
+          'username': 'try2',
+          'password':'me2'
+        });
+        var userRows = [row1, row2];
+        return db.insertOrReplace().into(dbReferences.userTable).values(userRows).exec();
+      }
+      // --- Populate Author table ----------------------------
+      var populateAuthors = function(){
+        var row1 = dbReferences.authorTable.createRow({
+          'id': 1,
+          'name': 'Isaac Asimov',
+          'pseudonym': 'Isaac Asimov'
+        });
+        var row2 = dbReferences.authorTable.createRow({
+          'id': 2,
+          'name': 'Robert A. Heinlein',
+          'pseudonym': 'Robert A. Heinlein'
+        });
+        var row3 = dbReferences.authorTable.createRow({
+          'id': 3,
+          'name': 'Arthur C. Clarke',
+          'pseudonym': 'Arthur C. Clarke'
+        });
+        var row4 = dbReferences.authorTable.createRow({
+          'id': 4,
+          'name': 'Poul Anderson',
+          'pseudonym': 'Poul Anderson'
+        });
+        var authorRows = [ row1, row2, row3, row4];
 
-      return db.insertOrReplace().into(dbReferences.userTable_).values(userRows).exec();
+        return db.insertOrReplace().into(dbReferences.authorTable).values(authorRows).exec();
+      }
+      // --- Populate Book table ----------------------------
+      var populateBooks = function(){
+        var row1 = dbReferences.bookTable.createRow({
+          'isbn': '8804519525',
+          'title': 'Io, robot',
+          'description': 'A lei, un robot è solo un robot. Ma lei non ha lavorato con loro. Lei non li conosce. Loro sono degli addetti alle pulizie, i migliori che abbiamo. Quando la Terra è dominata da un padrone-macchina... quando i robot sono più umani dell\'umanità. La visione indimenticabile, agghiacciante del futuro, da parte di Isaac Asimov - disponibile finalmente nella sua prima edizione in brossura.',
+          'imageUrl': 'https://images-na.ssl-images-amazon.com/images/I/41WFM04FSxL._SY344_BO1,204,203,200_.jpg',
+          'basePrice': 9.90,
+          'authorId': 1
+        });
+        var bookRows = [ row1 ];
+
+        return db.insertOrReplace().into(dbReferences.bookTable).values(bookRows).exec();
+      }
+      //var populateBooks = new Promise();
+      var populateDB = [populateUsers() , populateAuthors() , populateBooks()];
+
+      return Promise.all(populateDB);
     });
   }
 
+
+  /**
+  * Query the DB to find the username with corresponding password.
+  * @return {username.id} or {false}
+  */
   this.authenticate = function(username, password){
 
     var resultHandler = function(queryResult){
@@ -79,7 +131,7 @@ function DbManager($log, $filter){
       return false;
     }
 
-    var userTable = dbReferences.userTable_;
+    var userTable = dbReferences.userTable;
     var db = dbReferences.db;
 
     return db.select()
@@ -90,8 +142,9 @@ function DbManager($log, $filter){
       )
       .exec()
       .then(resultHandler);
-
   }
+
+
 
 
 
